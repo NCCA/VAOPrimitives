893bbb108d9d7fcb40cc4196dd802bbce72e6170
jmacey <jmacey@bournemouth.ac.uk>
2015-10-07 22:10:47 +0100

    removed the NGLQuit call

--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -33,10 +33,8 @@ NGLScene::NGLScene()
 
 NGLScene::~NGLScene()
 {
-  ngl::NGLInit *Init = ngl::NGLInit::instance();
   std::cout<<"Shutting down NGL, removing VAO's and Shaders\n";
   delete m_light;
-  Init->NGLQuit();
 }
 
 void NGLScene::resizeGL(int _w, int _h)
