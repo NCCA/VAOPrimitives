c700bf538d9e15010161294d7a543ee38d62cccf
jmacey <jmacey@bournemouth.ac.uk>
2018-09-11 19:42:39 +0100

    updated for NGL7

--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -2,9 +2,6 @@
 #include <QGuiApplication>
 
 #include "NGLScene.h"
-#include <ngl/Camera.h>
-#include <ngl/Light.h>
-#include <ngl/Material.h>
 #include <ngl/NGLInit.h>
 #include <ngl/VAOPrimitives.h>
 #include <ngl/ShaderLib.h>
@@ -26,11 +23,11 @@ NGLScene::~NGLScene()
 
 void NGLScene::resizeGL( int _w, int _h )
 {
-  m_cam.setShape( 45.0f, static_cast<float>( _w ) / _h, 0.05f, 350.0f );
+  m_project=ngl::perspective(45.0f, static_cast<float>( _w ) / _h, 0.05f, 350.0f );
   m_win.width  = static_cast<int>( _w * devicePixelRatio() );
   m_win.height = static_cast<int>( _h * devicePixelRatio() );
 }
-
+constexpr auto shaderProgram="PBR";
 void NGLScene::initializeGL()
 {
   // we need to initialise the NGL lib which will load all of the OpenGL functions, this must
@@ -40,70 +37,53 @@ void NGLScene::initializeGL()
   glClearColor(0.4f, 0.4f, 0.4f, 1.0f);			   // Grey Background
   // enable depth testing for drawing
   glEnable(GL_DEPTH_TEST);
+
+  m_lightAngle=0.0;
+  m_lightPos.set(sinf(m_lightAngle),2,cosf(m_lightAngle));
   // now to load the shader and set the values
   // grab an instance of shader manager
   ngl::ShaderLib* shader = ngl::ShaderLib::instance();
-  // we are creating a shader called Phong to save typos
+  // we are creating a shader called PBR to save typos
   // in the code create some constexpr
-  constexpr auto shaderProgram = "Phong";
-  constexpr auto vertexShader  = "PhongVertex";
-  constexpr auto fragShader    = "PhongFragment";
+  constexpr auto vertexShader  = "PBRVertex";
+  constexpr auto fragShader    = "PBRFragment";
   // create the shader program
   shader->createShaderProgram( shaderProgram );
   // now we are going to create empty shaders for Frag and Vert
   shader->attachShader( vertexShader, ngl::ShaderType::VERTEX );
   shader->attachShader( fragShader, ngl::ShaderType::FRAGMENT );
   // attach the source
-  shader->loadShaderSource( vertexShader, "shaders/PhongVertex.glsl" );
-  shader->loadShaderSource( fragShader, "shaders/PhongFragment.glsl" );
+  shader->loadShaderSource( vertexShader, "shaders/PBRVertex.glsl" );
+  shader->loadShaderSource( fragShader, "shaders/PBRFragment.glsl" );
   // compile the shaders
   shader->compileShader( vertexShader );
   shader->compileShader( fragShader );
   // add them to the program
   shader->attachShaderToProgram( shaderProgram, vertexShader );
   shader->attachShaderToProgram( shaderProgram, fragShader );
-
-
   // now we have associated that data we can link the shader
   shader->linkProgramObject( shaderProgram );
   // and make it active ready to load values
   ( *shader )[ shaderProgram ]->use();
-
-  // now we have associated this data we can link the shader
-  shader->linkProgramObject("Phong");
-  // and make it active ready to load values
-  (*shader)["Phong"]->use();
-  shader->setUniform("Normalize",1);
-  // the shader will use the currently active material and light0 so set them
-  ngl::Material m( ngl::STDMAT::GOLD );
-  // load our material values to the shader into the structure material (see Vertex shader)
-  m.loadToShader( "material" );
-
-  // Now we will create a basic Camera from the graphics library
-  // This is a static camera so it only needs to be set once
-  // First create Values for the camera position
-  ngl::Vec3 from(0.0f,2.0f,6.0f);
-  ngl::Vec3 to(0.0f,0.0f,0.0f);
-  ngl::Vec3 up(0,1,0);
+ // We now create our view matrix for a static camera
+  ngl::Vec3 from( 0.0f, 2.0f, 10.0f );
+  ngl::Vec3 to( 0.0f, 0.0f, 0.0f );
+  ngl::Vec3 up( 0.0f, 1.0f, 0.0f );
   // now load to our new camera
-  m_cam.set(from,to,up);
-  // set the shape using FOV 45 Aspect Ratio based on Width and Height
-  // The final two are near and far clipping planes of 0.5 and 10
-  m_cam.setShape(45,720.0f/576.0f,0.05f,350);
-  shader->setUniform("viewerPos",m_cam.getEye().toVec3());
+  m_view=ngl::lookAt(from,to,up);
+  shader->setUniform( "camPos", from );
+  // now a light
+  m_lightPos.set( 0.0, 2.0f, 2.0f ,1.0f);
+  // setup the default shader material and light porerties
+  // these are "uniform" so will retain their values
+  shader->setUniform("lightPosition",m_lightPos.toVec3());
+  shader->setUniform("lightColor",400.0f,400.0f,400.0f);
+  shader->setUniform("exposure",2.2f);
+  shader->setUniform("albedo",0.950f, 0.71f, 0.29f);
 
-  m_lightAngle=0.0;
-  m_light.reset( new ngl::Light(ngl::Vec3(sinf(m_lightAngle),2,cosf(m_lightAngle)),ngl::Colour(1,1,1,1),ngl::Colour(1,1,1,1),ngl::LightModes::POINTLIGHT));
-
-  // now create our light this is done after the camera so we can pass the
-  // transpose of the projection matrix to the light to do correct eye space
-  // transformations
-  ngl::Mat4 iv=m_cam.getViewMatrix();
-  iv.transpose();
-  m_light->setTransform(iv);
-  // load these values to the shader as well
-  m_light->loadToShader("light");
-
+  shader->setUniform("metallic",1.02f);
+  shader->setUniform("roughness",0.38f);
+  shader->setUniform("ao",0.2f);
 
   ngl::VAOPrimitives *prim=ngl::VAOPrimitives::instance();
   prim->createSphere("sphere",0.5f,50);
@@ -127,21 +107,25 @@ void NGLScene::initializeGL()
 
 void NGLScene::loadMatricesToShader()
 {
-  ngl::ShaderLib *shader=ngl::ShaderLib::instance();
+  ngl::ShaderLib* shader = ngl::ShaderLib::instance();
+    shader->use("PBR");
+    struct transform
+    {
+      ngl::Mat4 MVP;
+      ngl::Mat4 normalMatrix;
+      ngl::Mat4 M;
+    };
 
-  ngl::Mat4 MV;
-  ngl::Mat4 MVP;
-  ngl::Mat3 normalMatrix;
-  ngl::Mat4 M;
-  M  = m_mouseGlobalTX*m_transform.getMatrix();
-  MV = m_cam.getViewMatrix()*M;
-  MVP= m_cam.getProjectionMatrix()*MV;
-  normalMatrix=MV;
-  normalMatrix.inverse().transpose();
-  shader->setUniform("MV",MV);
-  shader->setUniform("MVP",MVP);
-  shader->setUniform("normalMatrix",normalMatrix);
-  shader->setUniform("M",M);
+     transform t;
+     t.M=m_view*m_mouseGlobalTX*m_transform.getMatrix();
+
+     t.MVP=m_project*t.M;
+     t.normalMatrix=t.M;
+     t.normalMatrix.inverse().transpose();
+     shader->setUniformBuffer("TransformUBO",sizeof(transform),&t.MVP.m_00);
+
+     shader->setUniform("lightPosition",(m_mouseGlobalTX*m_lightPos).toVec3());
+
 }
 
 void NGLScene::drawScene(const std::string &_shader)
@@ -304,7 +288,7 @@ void NGLScene::paintGL()
   // clear the screen and depth buffer
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glViewport(0,0,m_win.width,m_win.height);
-  drawScene("Phong");
+  drawScene("PBR");
 
 }
 
@@ -424,8 +408,7 @@ void NGLScene::updateLight()
   m_lightAngle+=0.1;
 
   // now set this value and load to the shader
-  m_light->setPosition(ngl::Vec3(4.0*cos(m_lightAngle),2,4.0*sin(m_lightAngle)));
-  m_light->loadToShader("light");
+  m_lightPos.set(ngl::Vec3(4.0*cosf(m_lightAngle),2.0f,4.0f*sinf(m_lightAngle)));
 }
 
 void NGLScene::timerEvent(QTimerEvent *_event )
